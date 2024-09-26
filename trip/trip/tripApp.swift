6e6843fd472b88170b0959dff8edeb348e7239b6
JayAgra <69493224+JayAgra@users.noreply.github.com>
2024-09-26 09:00:43 -0700

    empty project

---
+++ trip/trip/tripApp.swift
@@ -0,0 +1,17 @@
+//
+//  tripApp.swift
+//  trip
+//
+//  Created by Jayen Agrawal on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct tripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
