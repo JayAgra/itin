1dfeb3b69677155d55b02fa80a93d0098b574e9b
JayAgra <69493224+JayAgra@users.noreply.github.com>
2024-09-26 09:09:24 -0700

    trip list

---
+++ trip/trip/TripList.swift
@@ -0,0 +1,40 @@
+//
+//  TripList.swift
+//  Itin
+//
+//  Created by Jayen Agrawal on 9/26/24.
+//
+
+import SwiftUI
+
+struct TripList: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(0..<10) { trip in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("Trip to Japan")
+                                    .font(.title)
+                                Text("April 3-10, 2025")
+                                    .font(.caption)
+                            }
+                            Spacer()
+                            VStack {
+                                Text("189")
+                                    .font(.largeTitle)
+                                Text("days")
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Trips")
+        }
+    }
+}
+
+#Preview {
+    TripList()
+}
